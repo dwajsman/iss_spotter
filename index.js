4670f4c19bfd2aaf6f4833bef5549304c34ff0df
Daniel Wajsman <danielwajsman@mac.com>
2021-11-15 14:06:55 -0800

    with promisses

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ const {
 
 function prettyInfo(passTimes) {
   passTimes.forEach(pass => {
-    const datetime = new Date(0);
+    let datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
+    let duration = pass.duration;
     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   });
 }
